be389eeef316622c34a9fd710d9f7b3a3bb569f3
Andrey Ershov <ershov.one@gmail.com>
2023-07-06 12:48:39 +0800

    error handler

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.js';
+import { notFound, errorHandler } from './middleware/error.js';
 
 dotenv.config();
 const port = process.env.PORT || 8000;
@@ -10,6 +11,9 @@ const app = express();
 app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => res.send('Server is ready'));
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
  
