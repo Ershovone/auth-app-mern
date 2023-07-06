49e64671071634b8adbca00c44423d5c66f8d858
Andrey Ershov <ershov.one@gmail.com>
2023-07-06 13:02:43 +0800

    provide all api routes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,16 @@
 import express from 'express';
 const router = express.Router();
-import { authUser } from '../controllers/user.js';
+import {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile
+} from '../controllers/user.js';
 
+router.post('/', registerUser);
 router.post('/auth', authUser);
+router.post('/logout', logoutUser);
+router.route('/profile').get(getUserProfile).put(updateUserProfile);
 
 export default router;
