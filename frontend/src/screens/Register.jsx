7e64cb2290afa13cebb063ae410a0640b58d24be
Andrey Ershov <ershov.one@gmail.com>
2023-07-09 16:07:11 +0800

    register form

--- frontend/src/screens/Register.jsx
+++ frontend/src/screens/Register.jsx
@@ -1,7 +1,12 @@
-import { useState } from "react";
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import { Form, Button, Row, Col } from 'react-bootstrap';
+import { toast } from 'react-toastify';
 import FormContainer from '../components/FormContainer';
+import Loader from "../components/Loader";
+import { useRegisterMutation } from "../slices/usersApi";
+import { setCredentials } from "../slices/auth";
 
 function Register() {
   const [name, setName] = useState('');
@@ -9,10 +14,32 @@ function Register() {
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
 
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const [register, { isLoading }] = useRegisterMutation();
+
+  const { userInfo } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate('/');
+    }
+  }, [navigate, userInfo]);
+
   const submitHandler = async (e) => {
     e.preventDefault();
-
-    console.log('submit');
+    if (password !== confirmPassword) {
+      toast.error('Passwords do not match');
+    } else {
+      try {
+        const res = await register({ name, email, password }).unwrap();
+        dispatch(setCredentials({ ...res }));
+        navigate('/');
+      } catch (error) {
+        toast.error(error?.data?.message || error.error);
+      }
+    }
   };
 
   return (
@@ -56,6 +83,7 @@ function Register() {
             onChange={(e) => setConfirmPassword(e.target.value)}
           ></Form.Control>
         </Form.Group>
+        {isLoading && <Loader />}
         <Button type='submit' variant="primary" className="mt-3">
           Sign Up
         </Button>
